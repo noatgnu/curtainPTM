f41ff6ed954cd65a3c14b15f83d222364854b2dd
Toan Phung <toan.phungkhoiquoctoan@gmail.com>
2022-02-21 14:47:15 +0000

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,40 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import * as PlotlyJS from 'plotly.js-dist-min';
+import { PlotlyModule } from 'angular-plotly.js';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import { HeatmapComponent } from './components/heatmap/heatmap.component';
+import { HomeComponent } from './components/home/home.component';
+import { FileUploaderComponent } from './components/file-uploader/file-uploader.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { QuickSearchComponent } from './components/quick-search/quick-search.component';
+import { VolcanoPlotComponent } from './components/volcano-plot/volcano-plot.component';
+import { ProteinViewerComponent } from './components/protein-viewer/protein-viewer.component';
+import { BarChartComponent } from './components/bar-chart/bar-chart.component';
 
+PlotlyModule.plotlyjs = PlotlyJS;
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeatmapComponent,
+    HomeComponent,
+    FileUploaderComponent,
+    QuickSearchComponent,
+    VolcanoPlotComponent,
+    ProteinViewerComponent,
+    BarChartComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    PlotlyModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/classes/curtain-link.ts
@@ -0,0 +1,9 @@
+
+
+export class CurtainLink {
+  proxyURL: string = "/api/"
+  stringURL: string = "http://string-db.org/"
+  uniprotBASE: string = "https://www.uniprot.org/uploadlists/?"
+  ebiAlphaFoldURL: string = "https://alphafold.ebi.ac.uk/"
+  interactomeAtlas: string = "http://www.interactome-atlas.org/app.php/"
+}
