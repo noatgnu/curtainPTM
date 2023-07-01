e68b1fae5ca7742cf376d39a2164576e3d0ff970
Toan Phung <toan.phung@proteo.info>
2023-07-01 17:07:39 +0100

    performance update

---
+++ src/app/components/file-form/data.worker.ts
@@ -0,0 +1,176 @@
+/// <reference lib="webworker" />
+
+import {DataFrame, fromCSV, Series, IDataFrame} from "data-forge";
+
+addEventListener('message', (data: MessageEvent<any>) => {
+  console.log(data.data)
+  switch (data.data.task) {
+    case "processDifferentialFile":
+      postMessage({type: "progress", value: 100, text: "Processing differential data..."})
+      let df: IDataFrame = fromCSV(data.data.differential)
+
+      if (!data.data.differentialForm._comparison || data.data.differentialForm._comparison === "" || data.data.differentialForm._comparison === "CurtainSetComparison") {
+        data.data.differentialForm._comparison = "CurtainSetComparison"
+        data.data.differentialForm._comparisonSelect = "1"
+
+        df = df.withSeries("CurtainSetComparison", new Series(Array(df.count()).fill("1"))).bake()
+      }
+
+      if (data.data.differentialForm._comparisonSelect === "" || data.data.differentialForm._comparisonSelect === undefined) {
+        data.data.differentialForm._comparisonSelect = data.data.differential.df.first()[data.data.differentialForm._comparison]
+      }
+      const store: any[] = df.toArray().map((r: any) => {
+        r[data.data.differentialForm._position] = Number(r[data.data.differentialForm._position])
+        r[data.data.differentialForm._positionPeptide] = Number(r[data.data.differentialForm._positionPeptide])
+        r[data.data.differentialForm._foldChange] = Number(r[data.data.differentialForm._foldChange])
+        r[data.data.differentialForm._score] = Number(r[data.data.differentialForm._score])
+        r[data.data.differentialForm._significant] = Number(r[data.data.differentialForm._significant])
+        if (data.data.differentialForm._transformFC) {
+          if (r[data.data.differentialForm._foldChange] > 0) {
+            r[data.data.differentialForm._foldChange] = Math.log2(r[data.data.differentialForm._foldChange])
+          } else if (r[data.data.differentialForm._foldChange] < 0) {
+            r[data.data.differentialForm._foldChange] = -Math.log2(Math.abs(r[data.data.differentialForm._foldChange]))
+          } else {
+            r[data.data.differentialForm._foldChange] = 0
+          }
+        }
+        if (data.data.differentialForm._significant) {
+          r[data.data.differentialForm._significant] = Number(r[data.data.differentialForm._significant])
+        }
+        if (data.data.differentialForm._transformSignificant) {
+          r[data.data.differentialForm._significant] = -Math.log10(r[data.data.differentialForm._significant])
+        }
+        r[data.data.differentialForm._peptideSequence] = parseSequenceSingle(r[data.data.differentialForm._peptideSequence])
+        return r
+      })
+
+
+
+      // passing data back to main thread in chunks of 100 items each to avoid memory issues
+      /*const chunkSize = 100
+      const chunkNumber = Math.ceil(df.count() / chunkSize)
+      for (let i = 0; i < chunkNumber; i++) {
+        postMessage({type: "progress", value: i*100/chunkNumber, text: "Processing differential data..."})
+        postMessage({type: "resultDifferential", differential: df.skip(i*chunkSize).take(chunkSize).toArray()})
+      }*/
+      postMessage({type: "progress", value: 100, text: "Finished processing differential data"})
+      // @ts-ignore
+      const result = {type: "resultDifferential", differential: JSON.stringify(store)}
+      postMessage(result)
+
+      break
+  case "processRawFile":
+    postMessage({type: "progress", value: 100, text: "Processing primary data"})
+    let rawDF: IDataFrame = fromCSV(data.data.raw)
+    const totalSampleNumber = data.data.rawForm._samples.length
+    let sampleNumber = 0
+    const conditions: string[] = []
+    let colorPosition = 0
+    const colorMap: any = {}
+    const conditionOrder = data.data.settings.conditionOrder.slice()
+    let samples: string[] = []
+    if (conditionOrder.length > 0) {
+      for (const c of conditionOrder) {
+        for (const s of data.data.settings.sampleOrder[c]) {
+          samples.push(s)
+        }
+      }
+    } else {
+      samples = data.data.rawForm._samples.slice()
+    }
+    const sampleMap: any = {}
+    for (const s of samples) {
+      const condition_replicate = s.split(".")
+      const replicate = condition_replicate[condition_replicate.length-1]
+      const condition = condition_replicate.slice(0, condition_replicate.length-1).join(".")
+      if (!conditions.includes(condition)) {
+        conditions.push(condition)
+        if (colorPosition >= data.data.settings.defaultColorList.length) {
+          colorPosition = 0
+        }
+        colorMap[condition] = data.data.settings.defaultColorList[colorPosition]
+        colorPosition ++
+      }
+      if (!data.data.settings.sampleOrder[condition]) {
+        data.data.settings.sampleOrder[condition] = []
+      }
+      if (!data.data.settings.sampleOrder[condition].includes(s)) {
+        data.data.settings.sampleOrder[condition].push(s)
+      }
+
+      if (!(s in data.data.settings.sampleVisible)) {
+        data.data.settings.sampleVisible[s] = true
+      }
+      sampleMap[s] = {replicate: replicate, condition: condition}
+    }
+    const storeRaw = rawDF.toArray().map((r: any) => {
+      for (const s of samples) {
+        r[s] = Number(r[s])
+      }
+      return r
+    })
+
+    // @ts-ignore
+    postMessage({type: "resultRaw", raw: JSON.stringify(storeRaw), settings: data.data.settings, sampleMap: sampleMap})
+  }
+});
+
+function convertToNumber(arr: string[]) {
+  const newCol = arr.map(Number)
+  return newCol
+}
+
+function log2Convert(arr: number[]) {
+  const newCol = arr.map(a => log2Stuff(a))
+  return newCol
+}
+
+function log2Stuff(data: number) {
+  if (data > 0) {
+    return Math.log2(data)
+  } else if (data < 0) {
+    return -Math.log2(Math.abs(data))
+  } else {
+    return 0
+  }
+}
+
+function log10Convert(arr: number[]) {
+  const newCol = arr.map(a => -Math.log10(a))
+  return newCol
+}
+function parseSequence(df: IDataFrame, sequenceColumnName: string) {
+  return df.getSeries(sequenceColumnName).bake().toArray().map(v => {
+    let count = 0
+    let seq = ""
+    for (const a of v) {
+      if (["(", "[", "{"].includes(a)) {
+        count = count + 1
+      }
+      if (count === 0) {
+        seq = seq + a
+      }
+      if ([")", "]", "}"].includes(a)) {
+        count = count - 1
+      }
+    }
+    return seq
+  })
+}
+
+function parseSequenceSingle(v: string) {
+  let count = 0
+  let seq = ""
+  for (const a of v) {
+    if (["(", "[", "{"].includes(a)) {
+      count = count + 1
+    }
+    if (count === 0) {
+      seq = seq + a
+    }
+    if ([")", "]", "}"].includes(a)) {
+      count = count - 1
+    }
+  }
+  return seq
+}
--- src/app/components/file-form/file-form.component.ts
+++ src/app/components/file-form/file-form.component.ts
@@ -1,7 +1,7 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {InputFile} from "../../classes/input-file";
 import {DataService} from "../../data.service";
-import {Series} from "data-forge";
+import {DataFrame, fromCSV, fromJSON, Series} from "data-forge";
 import {UniprotService} from "../../uniprot.service";
 import {SettingsService} from "../../settings.service";
 
@@ -22,7 +22,67 @@ export class FileFormComponent implements OnInit {
     })
     this.data.restoreTrigger.asObservable().subscribe(data => {
       if (data) {
-        this.processFiles().then()
+        if (typeof Worker !== 'undefined') {
+          console.log("start worker")
+          // Create a new
+          const worker = new Worker(new URL('./data.worker', import.meta.url));
+          worker.onmessage = (data: MessageEvent<any>) => {
+            console.log(data.data)
+            if (data.data) {
+              if (data.data.type === "progress") {
+                this.updateProgressBar(data.data.value, data.data.text)
+              } else {
+                if (data.data.type==="resultDifferential") {
+                  this.data.differential.df = fromJSON(data.data.differential)
+                  const currentDF = this.data.differential.df.where(r => r[this.data.differentialForm.comparison] === this.data.differentialForm.comparisonSelect).resetIndex().bake()
+
+                  const fc = currentDF.getSeries(this.data.differentialForm.foldChange).where(i => !isNaN(i)).bake()
+                  const sign = currentDF.getSeries(this.data.differentialForm.significant).where(i => !isNaN(i)).bake()
+
+                  this.data.minMax = {
+                    fcMin: fc.min(),
+                    fcMax: fc.max(),
+                    pMin: sign.min(),
+                    pMax: sign.max()
+                  }
+                  this.data.currentDF = currentDF
+                  this.data.primaryIDsList = this.data.currentDF.getSeries(this.data.differentialForm.primaryIDs).bake().distinct().toArray()
+                  this.data.accessionList = this.data.currentDF.getSeries(this.data.differentialForm.accession).bake().distinct().toArray()
+                  for (const p of this.data.accessionList) {
+                    if (!this.data.accessionMap[p])  {
+                      this.data.accessionMap[p] = {}
+                      this.data.accessionMap[p][p] = true
+                    }
+                    for (const n of p.split(";")) {
+                      if (!this.data.accessionMap[n]) {
+                        this.data.accessionMap[n] = {}
+                      }
+                      this.data.accessionMap[n][p] = true
+                    }
+                  }
+                  worker.postMessage({task: 'processRawFile',rawForm: this.data.rawForm, raw: this.data.raw.originalFile, settings: settings.settings})
+                  this.data.raw.df = new DataFrame()
+                } else if (data.data.type==="resultRaw") {
+                  this.data.raw.df = fromJSON(data.data.raw)
+                  this.data.sampleMap = data.data.sampleMap
+                  this.settings.settings = data.data.settings
+                  this.processUniProt()
+                  worker.terminate()
+                } else if (data.data.type==="resultDifferentialCompleted") {
+
+                }
+              }
+            } else {
+              worker.terminate()
+            }
+
+          };
+          worker.postMessage({task: 'processDifferentialFile', differential: this.data.differential.originalFile, differentialForm: this.data.differentialForm});
+          this.data.differential.df = new DataFrame()
+        } else {
+          this.processFiles().then()
+        }
+
       }
     })
   }
@@ -68,6 +128,7 @@ export class FileFormComponent implements OnInit {
   }
 
   async processFiles() {
+
     if (!this.data.differentialForm.comparison || this.data.differentialForm.comparison === "" || this.data.differentialForm.comparison === "CurtainSetComparison") {
       this.data.differentialForm.comparison = "CurtainSetComparison"
       this.data.differentialForm.comparisonSelect = "1"
