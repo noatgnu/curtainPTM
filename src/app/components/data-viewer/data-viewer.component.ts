3168b4fab9353044fbd5141b814bb7e697f1040f
Toan Phung <toan.phung@proteo.info>
2023-11-26 20:03:28 +0000

    update

--- src/app/components/data-viewer/data-viewer.component.ts
+++ src/app/components/data-viewer/data-viewer.component.ts
@@ -4,6 +4,7 @@ import {FormBuilder} from "@angular/forms";
 import {DataService} from "../../data.service";
 import {debounceTime, distinctUntilChanged} from "rxjs";
 import {UniprotService} from "../../uniprot.service";
+import {ToastService} from "../../toast.service";
 
 @Component({
   selector: 'app-data-viewer',
@@ -12,16 +13,19 @@ import {UniprotService} from "../../uniprot.service";
 })
 export class DataViewerComponent implements OnInit {
   _data: ISeries<number, IDataFrame<number, any>> = new Series()
-
+  baseData: ISeries<number, IDataFrame<number, any>> = new Series()
   form = this.fb.group({
     filterTerm: [""],
     filterType: ["Gene Names"],
+    filterSearchOperation: ["All selected"],
   })
-
+  ready = false
 
   @Input() set data(value: ISeries<number, IDataFrame<number, any>>) {
     this._data = value
+    this.baseData = value
     this.displaySeries = value
+    this.ready = true
   }
 
   get data(): ISeries<number, IDataFrame<number, any>> {
@@ -29,7 +33,7 @@ export class DataViewerComponent implements OnInit {
   }
 
   displaySeries: ISeries<number, IDataFrame<number, any>> = new Series()
-  constructor(private fb: FormBuilder, public dataService: DataService, private uniprot: UniprotService) {
+  constructor(private fb: FormBuilder, public dataService: DataService, private uniprot: UniprotService, private toast: ToastService) {
     this.form.controls["filterTerm"].valueChanges.pipe(debounceTime(200), distinctUntilChanged()).subscribe((value) => {
       let primaryIds: string[] = []
       if (value){
@@ -45,7 +49,7 @@ export class DataViewerComponent implements OnInit {
               primaryIds = this.dataService.selected.filter((primaryID: string) => primaryID.toLowerCase().includes(value.toLowerCase()))
               break
             case "Diseases":
-              this._data.forEach((df: IDataFrame<number, any>) => {
+              this.baseData.forEach((df: IDataFrame<number, any>) => {
                 const acc = df.getSeries(this.dataService.differentialForm.accession).bake().toArray()[0]
                 const uni = this.uniprot.getUniprotFromAcc(acc)
                 if (uni["Involvement in disease"]) {
@@ -57,9 +61,9 @@ export class DataViewerComponent implements OnInit {
               break
           }
           if (value === "") {
-            this.displaySeries = this._data
+            this.displaySeries = this.baseData
           } else if (primaryIds.length > 0) {
-            this.displaySeries = this._data.where((df: IDataFrame<number, any>) => {
+            this.displaySeries = this.baseData.where((df: IDataFrame<number, any>) => {
               const s = df.getSeries(this.dataService.differentialForm.primaryIDs).bake()
               return s.bake().any((primaryID: string) => {
                 return primaryIds.includes(primaryID)
@@ -70,9 +74,38 @@ export class DataViewerComponent implements OnInit {
           }
         }
       } else {
-        this.displaySeries = this._data
+        this.displaySeries = this.baseData
       }
 
+    })
+
+    this.form.controls.filterSearchOperation.valueChanges.subscribe((value) => {
+      this.ready = false
+      this.toast.show("Filtering", "Filtering data for " + value, 5000).then()
+      console.log(this.displaySeries.count(), this.baseData.count(), this.data.count())
+      if (value) {
+        const result = []
+        const toBeFiltered: string[] = []
+        if (value === "All selected") {
+          this.baseData = this._data
+        } else if (this.dataService.selectOperationNames.includes(value)) {
+          for (const i in this.dataService.selectedMap) {
+            if (this.dataService.selectedMap[i][value]) {
+              toBeFiltered.push(i)
+            }
+          }
+          this.baseData = this._data.where((df: IDataFrame<number, any>) => {
+            const s = df.getSeries(this.dataService.differentialForm.primaryIDs).bake()
+            return s.any((primaryID: string) => {
+              return toBeFiltered.includes(primaryID)
+            })
+          }).bake()
+        }
+      }
+      this.toast.show("Filtering", "Completed filtering data for " + value, 5000).then()
+      this.displaySeries = this.baseData
+      console.log(this.displaySeries.count(), this.baseData.count(), this.data.count())
+      this.ready = true
     })
   }
 
