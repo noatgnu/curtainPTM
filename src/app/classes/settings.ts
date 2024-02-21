24c53207e27644e63ae8f980eb4b2ebdabcc5c09
Toan Phung <toan.phung@proteo.info>
2024-02-21 15:26:51 +0000

    update

--- src/app/classes/settings.ts
+++ src/app/classes/settings.ts
@@ -22,7 +22,7 @@ export class Settings {
   sampleOrder: any = {}
   sampleVisible: any = {}
   conditionOrder: string[] = []
-  volcanoAxis: any = {minX: null, maxX: null, minY: null, maxY: null, x: "-log10(p-value)", y: "Log2FC"}
+  volcanoAxis: any = {minX: null, maxX: null, minY: null, maxY: null, x: "Log2FC", y: "-log10(p-value)"}
   barchartColorMap: any = {}
   textAnnotation: any = {}
   volcanoPlotTitle: string = ""
